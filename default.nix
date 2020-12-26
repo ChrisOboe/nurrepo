f74702f633621a8ecea09d8a02ef602795170a83
Chris Oboe <chris@oboe.email>
2020-12-26 20:32:40 +0100

    add arcan and chitubox derivations

--- default.nix
+++ default.nix
@@ -14,8 +14,8 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  #example-package = pkgs.callPackage ./pkgs/example-package { };
+  chitubox = pkgs.callPackage ./pkgs/chitubox/chitubox.nix {} ;
+  arcan = pkgs.callPackage ./pkgs/arcan/arcan.nix {} ;
 }
 
